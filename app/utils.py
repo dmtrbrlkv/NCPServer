f5b8f1c752b6d49d55f555cc7a18fb15f274c3d2
Dmitry Burlakov <dmtrbrlkv@gmail.com>
2020-04-19 16:44:05 +0300

    setup

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from notescompy import session
 import logging
-from flask import Flask
+from flask import Flask, jsonify, request
 import json
 
 
@@ -80,3 +80,140 @@ def init():
     if "password" in config:
         del config["password"]
     return app, config, base_url, databases, separator
+
+
+def create_routs(base_url, app, databases, separator):
+    @app.route(base_url + '/')
+    def index():
+        return_databases = {}
+        for dbname, config in databases.items():
+            return_config = {}
+
+            method_config = databases[dbname]["methods"]
+            doc_info_config = databases[dbname]["doc_info"]
+
+            if "view" in method_config:
+                method_config["view"] = [v for v in method_config["view"]]
+
+            return_config["methods"] = method_config
+            return_config["doc_info"] = doc_info_config
+
+            return_databases[dbname] = return_config
+
+        return jsonify(return_databases)
+
+    @app.route(base_url + '/<dbname>/view/<viewname>')
+    def view(dbname, viewname):
+        check = check_method(dbname, "view", databases)
+
+        if check:
+            return check, 403
+
+        view_config = databases[dbname]["methods"]["view"]
+        if viewname not in view_config:
+            return f"Not available view {viewname}", 403
+
+        try:
+            db = get_db(dbname, databases)
+        except:
+            app.logger.exception(dbname)
+            return f"Database {dbname} temporary not available", 500
+
+        view = db.get_view(view_config[viewname])
+        if not view:
+            app.logger.error(f"View {view_config[viewname]} from {db} not available")
+            return f"View {viewname} temporary not available", 500
+
+        if "keys" in request.headers:
+            keys = request.headers["keys"]
+            keys = keys.split(separator)
+        else:
+            keys = None
+
+        fields, properties, formulas = get_FPF(dbname, request.headers, databases, separator)
+
+        if keys:
+            try:
+                col = view.get_all_documents_by_key(keys)
+            except:
+                app.logger.exception(" * Bad request")
+                return f"Bad request, check keys", 400
+            data = col.get_values(fields=fields, properties=properties, formulas=formulas)
+        else:
+            data = view.get_values()
+
+        return jsonify(data)
+
+    @app.route(base_url + '/<dbname>/search')
+    def search(dbname):
+        check = check_method(dbname, "search", databases)
+
+        if check:
+            return check, 403
+
+        if "search_formula" in request.headers:
+            search_formula = request.headers["search_formula"]
+        else:
+            return f"Bad request, search formula not specified", 400
+
+        try:
+            db = get_db(dbname, databases)
+        except:
+            app.logger.exception(dbname)
+            return f"Database {dbname} temporary not available", 500
+
+        try:
+            col = db.search(search_formula)
+        except:
+            app.logger.exception(" * Bad request")
+            return f"Bad request, check search formula", 400
+
+        fields, properties, formulas = get_FPF(dbname, request.headers, databases, separator)
+
+        try:
+            data = col.get_values(fields=fields, properties=properties, formulas=formulas)
+            return jsonify(data)
+        except:
+            app.logger.exception(" * Bad request")
+            return f"Bad request, check properties and formulas", 400
+
+    @app.route(base_url + '/<dbname>/document/<doc_id>')
+    def document(dbname, doc_id):
+        check = check_method(dbname, "document", databases)
+
+        if check:
+            return check, 403
+
+        try:
+            db = get_db(dbname, databases)
+        except:
+            app.logger.exception(dbname)
+            return f"Database {dbname} temporary not available", 500
+
+        doc = db.get_document_by_unid(doc_id)
+
+        if doc:
+            fields, properties, formulas = get_FPF(dbname, request.headers, databases, separator)
+
+            try:
+                data = doc.get_values(fields=fields, properties=properties, formulas=formulas)
+                return jsonify(data)
+            except:
+                app.logger.exception(" * Bad request")
+                return f"Bad request, check properties and formulas", 400
+
+        return f"Document {doc_id} not available", 404
+
+    @app.route(base_url + '/<dbname>')
+    def db_info(dbname):
+        if dbname not in databases:
+            return "Not available db " + dbname
+
+        try:
+            db = get_db(dbname, databases)
+        except:
+            app.logger.exception(dbname)
+            return f"Database {dbname} temporary not available", 500
+
+        res = {"Documents": db.all_documents.count, "Size": db.size}
+        return jsonify(res)
--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup
 setup(
     name='NCPServer',
     version='0.1',
-    packages=[''],
-    url='',
+    scripts=['app\\app.py', 'app\\utils.py', 'config.json', 'config_example.json'],
+    url='https://github.com/dmtrbrlkv/NCPServer',
     license='',
     author='Dmitry Burlakov',
     author_email='dmtrbrlkv@gmail.com',
-    description=''
+    description='Simple server based on Flask and NotesComPy library,',
+    install_requires=['Flask'],
+
+    entry_points={
+        'console_scripts': [
+            'nspserver=app:main',
+        ],
+    }
+
 )
