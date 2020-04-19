82e05058b9081bc991f21a7cb52ec80a598b3315
Dmitry Burlakov <dmtrbrlkv@gmail.com>
2020-04-19 14:36:02 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NCPServer',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Dmitry Burlakov',
+    author_email='dmtrbrlkv@gmail.com',
+    description=''
+)
